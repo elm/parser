33c98fc8159fe4fd7d726b25036c8a3dad793e8a
Ben Burdette <bburdette@gmail.com>
2018-09-13 22:19:30 -0600

    wups, fix these messages!

--- src/Parser.elm
+++ src/Parser.elm
@@ -170,6 +170,7 @@ deadEndsToString : List DeadEnd -> String
 deadEndsToString deadEnds =
   String.concat (List.intersperse "; " (List.map deadEndToString deadEnds))
 
+
 deadEndToString : DeadEnd -> String
 deadEndToString deadend = 
   problemToString deadend.problem ++ " at row " ++ String.fromInt deadend.row ++ ", col " ++ String.fromInt deadend.col
@@ -180,11 +181,11 @@ problemToString p =
   case p of 
    Expecting s -> "expecting '" ++ s ++ "'"
    ExpectingInt -> "expecting int" 
-   ExpectingHex -> "expecting octal" 
+   ExpectingHex -> "expecting hex" 
    ExpectingOctal -> "expecting octal" 
    ExpectingBinary -> "expecting binary" 
-   ExpectingFloat -> "expecting number" 
-   ExpectingNumber -> "expecting variable" 
+   ExpectingFloat -> "expecting float" 
+   ExpectingNumber -> "expecting number" 
    ExpectingVariable -> "expecting variable" 
    ExpectingSymbol s -> "expecting symbol '" ++ s ++ "'"
    ExpectingKeyword s -> "expecting keyword '" ++ s ++ "'"
